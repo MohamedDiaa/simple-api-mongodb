d495f5ed955c1cc702e09054e2cd758d91336a8f
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-02-12 15:52:09 +0200

    add route functions getCars addCar and implment them

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from 'express';
 
+// import route functions
+import { getCars, addCar } from `./src/cars.js`
+
 // create a new server instance
 const app = express();
 
 // defining the port
 const PORT = 3333;
 
+// will recieve json data
+app.use(express.json());
+
 // define the routes
+app.get('/cars', getCars);
+
+app.post('/cars', addCar);
+
+// listen to the server on the specified port
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
